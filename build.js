9280c8e81750c59c878d8de5135b28c472d88164
thomasandersen <thomas@drivdigital.no>
2015-12-17 10:18:47 +0100

    Cleaned up the build script

--- build.js
+++ build.js
@@ -1,54 +1,69 @@
+/* globals require, __dirname, mkdir, sed, cd, cp, echo, cat, exec, rm, exit */
+
 require( 'shelljs/make' );
 
 const PLUGIN_NAME = 'woocommerce-bring-fraktguiden';
-const SRC_DIR = './src';
-const RELEASE_DIR = './release';
-const TMP_DIR = './tmp';
+const SRC_DIR = 'src';
+const RELEASE_DIR = 'release';
+const TEMP_DIR = 'tmp/woocommerce-bring-fraktguiden';
 
-target.all = function () {
+/**
+ * Build targets
+ * @type {{release: release, publish: publish, clean: Function}}
+ */
+target = {
+    release: release,
+    publish: publish,
+    clean:   function () {
+        clean( true )
+    }
 };
 
-target.release = function () {
-    clean(true);
-
-    if ( !test( '-d', TMP_DIR ) ) {
-        mkdir( TMP_DIR );
-    }
-    if ( !test( '-d', RELEASE_DIR ) ) {
-        mkdir( RELEASE_DIR );
-    }
-
-    var dir = TMP_DIR + '/' + PLUGIN_NAME;
-
-    mkdir( dir );
-
-    cp( '-R', SRC_DIR + '/', dir );
-
+function release() {
     var versionNumber = getVersionNumber();
+    var zipFileName = PLUGIN_NAME + '-' + versionNumber + '.zip';
 
-    sed( '-i', '##VERSION##', versionNumber, dir + '/woocommerce-bring-fraktguiden.php' );
-    sed( '-i', '##VERSION##', versionNumber, dir + '/readme.txt' );
-
-    cd( TMP_DIR );
-    var zipfile = '../' + RELEASE_DIR + '/' + PLUGIN_NAME + '-' + versionNumber + '.zip';
-    exec( 'zip -r ' + zipfile + ' ' + PLUGIN_NAME );
-    cd( '../' );
-    //clean();
-};
-
-target.clean = function () {
     clean( true );
-};
+
+    // 1. Create the directories used for the build process.
+    mkdir( '-p', TEMP_DIR );
+    mkdir( RELEASE_DIR );
+
+    // 2. Copy the source files to the temporary directory.
+    cp( '-R', SRC_DIR + '/', TEMP_DIR );
+
+    // 3. Replace occurences of the version macro with the version number found in the plugin readme file.
+
+    sed( '-i', '##VERSION##', versionNumber, TEMP_DIR + '/woocommerce-bring-fraktguiden.php' );
+    sed( '-i', '##VERSION##', versionNumber, TEMP_DIR + '/readme.txt' );
+
+    // 4. Create the zip.
+    cd( TEMP_DIR );
+    // :)
+    cd( '../');
+    exec( 'zip -r ' + '../' + RELEASE_DIR + '/' + zipFileName + ' ' + PLUGIN_NAME );
+
+    cp( '-R', TEMP_DIR, RELEASE_DIR );
+    clean();
+}
+
+function publish() {
+    cd( __dirname + '/' + RELEASE_DIR );
+    mkdir('svn');
+    cd('svn');
+    exec('svn co https://plugins.svn.wordpress.org/bring-fraktguiden-for-woocommerce/ .');
+}
 
 function clean( all ) {
-    rm( '-rf', TMP_DIR );
+    cd(__dirname);
+    rm( '-rf', 'tmp' );
     if ( all ) {
         rm( '-rf', RELEASE_DIR );
     }
 }
 
 function getVersionNumber() {
-    var readmeFile = TMP_DIR + '/' + PLUGIN_NAME + '/readme.txt';
+    var readmeFile = SRC_DIR + '/readme.txt';
     var contents = cat( readmeFile );
     var parts = contents.split( '== Changelog ==' );
     if ( parts.length != 2 ) {
@@ -58,7 +73,7 @@ function getVersionNumber() {
 
     var changeLog = parts[1];
     var logs = changeLog.match( /^= \d\.\d\.\d(|[-|\s|\w]+) =/gm );
-    if ( !logs ) {
+    if ( ! logs ) {
         echo( 'Could not find a version number. Aborting.' );
         exit( 1 );
     }
