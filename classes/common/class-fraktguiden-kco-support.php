fc6c9040f1f7edfcfc0069d1e6f93adeed390708
Eivin Landa <landa@drivdigital.no>
2018-05-22 16:24:40 +0200

    Also set country in session when making a post-code lookup

--- classes/common/class-fraktguiden-kco-support.php
+++ classes/common/class-fraktguiden-kco-support.php
@@ -58,7 +58,8 @@ class Fraktguiden_KCO_Support {
     }
     // Set the billing and shipping code
     WC()->customer->set_shipping_postcode( $_REQUEST['post_code'] );
-    WC()->customer->set_billing_postcode(  $_REQUEST['post_code'] );
+    WC()->customer->set_billing_postcode( $_REQUEST['post_code'] );
+    WC()->customer->set_billing_country( $_REQUEST['country'] );
     wp_send_json( $data );
   }
 
