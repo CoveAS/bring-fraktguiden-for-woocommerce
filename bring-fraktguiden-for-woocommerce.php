771d2471828d949f85041eeba7f79974a00ee6f8
Eivin Landa <landa@drivdigital.no>
2018-06-14 11:35:56 +0200

    Bumped the version number

--- bring-fraktguiden-for-woocommerce.php
+++ bring-fraktguiden-for-woocommerce.php
@@ -14,12 +14,12 @@ define( 'FRAKTGUIDEN_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
  * Author:              Driv Digital AS
  * Author URI:          https://drivdigital.no/
  *
- * Version:             1.5.0
+ * Version:             1.5.2
  * Requires at least:   4.9.1
  * Tested up to:        4.9.2
  *
- * WC requires at least: 3.2.4
- * WC tested up to: 3.2.6
+ * WC requires at least: 3.4.0
+ * WC tested up to: 3.4.2
  *
  * Text Domain:         bring-fraktguiden
  * Domain Path:         /languages
@@ -30,7 +30,7 @@ define( 'FRAKTGUIDEN_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
  */
 class Bring_Fraktguiden {
 
-  const VERSION = '1.5.0';
+  const VERSION = '1.5.2';
 
   const TEXT_DOMAIN = Fraktguiden_Helper::TEXT_DOMAIN;
 
--- readme.txt
+++ readme.txt
@@ -7,9 +7,9 @@ Tags: woocommerce, shipping, bring, fraktguiden
 Requires at least: 4.5
 Tested up to: 4.9.1
 Requires PHP: 5.6
-WC requires at least: 3.1
-WC tested up to: 3.2.1
-Stable tag: 1.5.0
+WC requires at least: 3.4.0
+WC tested up to: 3.4.2
+Stable tag: 1.5.2
 
 Bring Fraktguiden provides shipping calculation based on rates from bring.no.
 
@@ -91,6 +91,12 @@ Yes, yes we do. [Bring Github](https://github.com/drivdigital/bring-fraktguiden-
 
 
 == Changelog ==
+
+= 1.5.2
+* Added compability with WooCommerce 3.4.x
+* Fixed bring booking issue after integrating mailbox packages
+* Fixed the calculate by weight option
+* Fixed shipping calculation for variations
 
 = 1.5.0
 * Fixed: BOOK-INPUT-020: Invalid product ID
