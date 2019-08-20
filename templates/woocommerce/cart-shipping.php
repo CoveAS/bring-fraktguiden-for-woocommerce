6d230d59c20bf468525ba89649790961bdf679aa
Michal Lukasik <michal@drivdigital.no>
2019-08-20 12:50:01 +0200

    BRING-1 Added custom shipping option template in order to expand shipping rate information

--- templates/woocommerce/cart-shipping.php
+++ templates/woocommerce/cart-shipping.php
@@ -25,8 +25,8 @@ $show_shipping_calculator = ! empty( $show_shipping_calculator );
 $calculator_text          = '';
 ?>
 <tr class="woocommerce-shipping-totals shipping">
-	<th colspan="2"><?php echo wp_kses_post( $package_name ); ?></th>
-	<td colspan="2">data-title="<?php echo esc_attr( $package_name ); ?>">
+	<th><?php echo wp_kses_post( $package_name ); ?></th>
+	<td data-title="<?php echo esc_attr( $package_name ); ?>">
 		<?php if ( $available_methods ) : ?>
 			<ul id="shipping_method" class="woocommerce-shipping-methods">
 				<?php foreach ( $available_methods as $method ) : ?>
