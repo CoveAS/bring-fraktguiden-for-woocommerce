b0c1a61fae278580b689e558708b3ef4737e3de0
Eivin Landa <eivin@cove.no>
2021-06-03 11:31:15 +0200

    Bumped the version number

--- bring-fraktguiden-for-woocommerce.php
+++ bring-fraktguiden-for-woocommerce.php
@@ -6,7 +6,7 @@
  * Author:              Cove AS
  * Author URI:          https://cove.no/
  *
- * Version:             1.8.3
+ * Version:             1.8.4
  * Requires at least:   4.9.1
  * Tested up to:        5.4.1
  *
