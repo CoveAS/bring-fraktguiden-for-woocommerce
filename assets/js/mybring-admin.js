d95119aa1513e22fd43203004cf8e5b3ae0c8e7c
George Burduli <burduli94@gmail.com>
2022-04-01 17:50:47 +0400

    zip code validation for Norway, Sweden, Denmark

--- assets/js/mybring-admin.js
+++ assets/js/mybring-admin.js
@@ -1,3 +1,5 @@
+import { isValid } from 'swedish-postal-code-validator';
+
 const form = document.querySelector('#mainform')
 const inputs = [...document.querySelectorAll('input[id^=woocommerce_bring_fraktguiden_booking_address]')]
 const bookingStatus = document.querySelector('#woocommerce_bring_fraktguiden_booking_enabled')
@@ -12,6 +14,33 @@ form.addEventListener('submit', e => {
 				e.preventDefault()
 				input.style.borderColor = "red";
 			}
+		}
+	}
+})
+
+jQuery("#woocommerce_bring_fraktguiden_booking_address_postcode").on('keyup', function() {
+	const countryInput = jQuery("#select2-woocommerce_bring_fraktguiden_booking_address_country-container").text()
+	if ( countryInput === "Norway" ) {
+		let isValidZip = /^\d{4}$/.test(this.value)
+		if (isValidZip) {
+			this.style.borderColor = "green";
+		} else {
+			this.style.borderColor = "red";
+		}
+	}
+	else if ( countryInput === "Sweden" ) {
+		if ( isValid(this.value) ) {
+			this.style.borderColor = "green";
+		} else {
+			this.style.borderColor = "red";
+		}
+	}
+	else if ( countryInput === "Denmak" ) {
+		let isValidZip = /^(?:[1-24-9]\d{3}|3[0-8]\d{2})$/.test(this.value)
+		if (isValidZip) {
+			this.style.borderColor = "green";
+		} else {
+			this.style.borderColor = "red";
 		}
 	}
 })
