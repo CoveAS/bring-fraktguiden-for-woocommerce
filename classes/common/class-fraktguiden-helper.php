aa43efb66f328af18f75617fe1137472d93602e0
Eivin Landa <eivin@cove.no>
2022-09-14 11:54:29 +0200

    Patched pirate vulnerability in the plugin

--- classes/common/class-fraktguiden-helper.php
+++ classes/common/class-fraktguiden-helper.php
@@ -403,6 +403,10 @@ class Fraktguiden_Helper {
 				return $message . ' ' .__( 'Please ensure you have a valid license to continue using PRO.', 'bring-fraktguiden-for-woocommerce' ) . '<br>'
 				. self::get_pro_terms_link( __( 'Click here to buy a license', 'bring-fraktguiden-for-woocommerce' ) );
 			}
+			if ( $days > 8 ) {
+				return sprintf( __( "☠️ Ahoy, matey! we understand that there ye don't want to, or fer other reasons be unable pay fer our plugin. Please kindly consider gettin' a license at a later time if ye enjoy usin' it.", 'bring-fraktguiden-for-woocommerce' ), "$days " . _n( 'day', 'days', $days, 'bring-fraktguiden-for-woocommerce' ) ) . '<br>'
+					. self::get_pro_terms_link( __( 'Click here to buy a license', 'bring-fraktguiden-for-woocommerce' ) );
+			}
 
 			/* translators: %s: Number of days */
 			return sprintf( __( 'Bring Fraktguiden PRO license has not yet been activated. You have %s remaining before PRO features are disabled.', 'bring-fraktguiden-for-woocommerce' ), "$days " . _n( 'day', 'days', $days, 'bring-fraktguiden-for-woocommerce' ) ) . '<br>'
