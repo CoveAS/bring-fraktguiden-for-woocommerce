d5d91b98fb6aea648a81889fdcf659b16366f2f6
Eivin Landa <eivin@cove.no>
2023-10-30 14:59:13 +0100

    Updated tested to versions

--- bring-fraktguiden-for-woocommerce.php
+++ bring-fraktguiden-for-woocommerce.php
@@ -1,24 +1,24 @@
 <?php
 /**
- * Plugin Name:         Bring Fraktguiden for WooCommerce
- * Plugin URI:          https://bringfraktguiden.no/
- * Description:         Bring Fraktguiden for WooCommerce makes it easy for your customers to choose delivery methods from Bring.
- * Author:              Cove AS
- * Author URI:          https://cove.no/
+ * Plugin Name:          Bring Fraktguiden for WooCommerce
+ * Plugin URI:           https://bringfraktguiden.no/
+ * Description:          Bring Fraktguiden for WooCommerce makes it easy for your customers to choose delivery methods from Bring.
+ * Author:               Cove AS
+ * Author URI:           https://cove.no/
  *
- * Version:             1.9.3
- * Requires at least:   5.6.0
- * Tested up to:        6.0.1
+ * Version:              1.9.3
+ * Requires at least:    5.6.0
+ * Tested up to:         6.4.0
  *
  * WC requires at least: 4.8.0
- * WC tested up to: 6.7.0
+ * WC tested up to:      8.2.1
  *
- * Text Domain:         bring-fraktguiden-for-woocommerce
- * Domain Path:         /languages
+ * Text Domain:          bring-fraktguiden-for-woocommerce
+ * Domain Path:          /languages
  *
- * @package             WooCommerce
- * @category            Shipping Method
- * @author              Bring Fraktguiden for WooCommerce
+ * @package              WooCommerce
+ * @category             Shipping Method
+ * @author               Bring Fraktguiden for WooCommerce
  */
 
 use Automattic\WooCommerce\Utilities\FeaturesUtil;
@@ -46,11 +46,11 @@ add_action(
 
 if (
 	isset($_GET['license-please'])
-	&& $_GET['license-please'] === 'bring-fraktguiden'
+	&& $_GET['license-please'] === 'bring-fraktguiden-for-woocommerce'
 ) {
 	try {
 		Fraktguiden_License::get_instance()->check_license();
-		$valid_to = get_option('bring_fraktguiden_pro_valid_to');
+		$valid_to = get_option('bring_fraktguiden_pro_valid_to', 0);
 		wp_send_json(['valid_to' => $valid_to]);
 	} catch (Exception $e) {
 		// Do nothing
