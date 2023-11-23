28a9e07253199a341ccaea1fff21d9a7948b3735
Eivin Landa <eivin@cove.no>
2023-11-23 15:57:21 +0100

    🧹💨

--- pro/pickuppoint/SelectedPickUpPointComponent.php
+++ pro/pickuppoint/SelectedPickUpPointComponent.php
@@ -9,7 +9,7 @@ class SelectedPickUpPointComponent {
 
 	public function render(): string {
 		$change = esc_html__('Change', 'bring-fraktguiden-for-woocommerce');
-		$map_label  = __( 'View on map', 'bring-fraktguiden-for-woocommerce' );
+		$map_label  = esc_html__( 'View on map', 'bring-fraktguiden-for-woocommerce' );
 		$hidden = $this->hidden ? 'style="display:none"' : '';
 		return <<<HTML
 <div class="bring-fraktguiden-pick-up-point-picker" data-max="{$this->number}" {$hidden}>
