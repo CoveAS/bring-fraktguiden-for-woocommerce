e367714882f28e474bbc125f7ea51ebfb751a742
Eivin Landa <eivin@cove.no>
2025-01-09 15:24:35 +0100

    Made pick up points work on classic checkout too

--- pro/assets/js/pick-up-point-checkout.js
+++ pro/assets/js/pick-up-point-checkout.js
@@ -1,8 +1,13 @@
+
 (function ($) {
 	// Assign data from localised js object
 	let pickUpPoints = window._fraktguiden_data.pick_up_points;
 	let selectedPickUpPoint = window._fraktguiden_data.selected_pick_up_point;
 	let requireUpdate = false;
+
+	// Global picker element variable
+	let pickerEl;
+
 
 	/**
 	 * jQuery UI block arguments
@@ -17,16 +22,149 @@
 	};
 
 
-	const modalEl = document.createElement('pick-up-points-modal');
-	document.body.appendChild(modalEl);
+	/**
+	 * Pick Up Point Modal
+	 */
+	class PickUpPointsModal extends HTMLElement {
+		constructor() {
+			super();
+			this.attachShadow({mode: 'open'});
 
-	let pickerEl;
-	let listEl;
+			// Create styles for the modal
+			const styles = document.createElement('style');
+			styles.textContent = _fraktguiden_data.pick_up_point_modal_css;
+
+			const el = document.createElement('div');
+			el.classList.add('bring-fraktguiden-pick-up-points-modal');
+			el.innerHTML = `
+			  <div class="bfg-pupm__wrap">
+				<div class="bfg-pupm__inner">
+					<div class="bfg-pupm__header">
+						<div class="bfg-pupm__instruction">
+							${_fraktguiden_data.i18n.MODAL_INSTRUCTIONS}
+						</div>
+						<div class="bfg-pupm__close" tabindex="0">&times;</div>
+					</div>
+					<div class="bfg-pupm__list"></div>
+				</div>
+			  </div>
+			`;
+
+			// Append styles and modal to the shadow DOM
+			this.shadowRoot.append(styles, el);
+
+			const jqEl = $(el);
+			// Create the modal structure
+			// Close modal when clicking on the close button ✖️
+			jqEl.find('.bfg-pupm__close').on('click', (e) => {
+				e.preventDefault();
+				this.close();
+			}).on('keyup', (e) => {
+				if (e.key !== 'Enter' && e.key !== ' ') {
+					return;
+				}
+				e.preventDefault();
+				this.close();
+			})
+
+			// Close modal when clicking on backdrop or the [esc] key
+			jqEl.on('click', (e) => {
+				e.preventDefault();
+				this.close();
+			}).on('keyup', (e) => {
+				if (e.key !== 'Escape') {
+					return;
+				}
+				e.preventDefault();
+				this.close();
+			});
+
+			// Prevent closing when clicking on inner elements
+			jqEl.find('.bfg-pupm__inner').on(
+				'click',
+				function (e) {
+					e.preventDefault();
+					e.stopPropagation();
+				}
+			);
+		}
+
+		// Open the modal
+		open() {
+			const el = this.shadowRoot.querySelector('.bring-fraktguiden-pick-up-points-modal');
+			el.classList.add('open');
+
+			const listItems = $(el).find('.bfg-pupm__item')
+			// Find selected pick up point and focus it
+			let selectedItems = listItems.filter(function () {
+				return $(this).data('id') === selectedPickUpPoint.id;
+			});
+			if (!selectedItems.length) {
+				selectedItems = listItems.first().focus();
+			}
+			selectedItems.focus();
+			setTimeout(function () {
+				selectedItems.focus();
+			}, 100);
+		}
+
+		// Close the modal
+		close() {
+			this.shadowRoot.querySelector('.bring-fraktguiden-pick-up-points-modal').classList.remove('open');
+		}
+
+		setError(text) {
+			$(this.shadowRoot.querySelector('.bfg-pupm__list')).text(text);
+		}
+
+		// Add pick-up points dynamically
+		setPickUpPoints(points, callback) {
+			const listContainer = this.shadowRoot.querySelector('.bfg-pupm__list');
+			listContainer.innerHTML = '';
+
+			points.forEach((point) => {
+				const address = utility.formatAddress(point);
+				const pointEl = $(`
+					<div class="bfg-pupm__item">
+						<div class="bfg-pupm__name">${point.name}</div>
+						<div class="bfg-pupm__address">${address}</div>
+					</div>
+				`);
+				pointEl.data('id', point.id);
+				pointEl.on('click', callback(point));
+				pointEl.on('keyup', function (e) {
+					if (e.key === 'ArrowUp') {
+						clone.prev().focus();
+						e.preventDefault();
+						return;
+					}
+					if (e.key === 'ArrowDown') {
+						clone.next().focus();
+						e.preventDefault();
+						return;
+					}
+
+					if (e.key !== 'Enter' && e.key !== ' ') {
+						return;
+					}
+					callback(point)(e);
+				});
+				listContainer.appendChild(pointEl[0]);
+			});
+		}
+	}
+
+
+
 	let getRequest = undefined;
 
+	/**
+	 * Utility
+	 * General purpose functions used by both the block and classic checkout
+	 */
 	const utility = {
 		refreshPickUpPoints: function () {
-			if (! requireUpdate) {
+			if (!requireUpdate) {
 				return;
 			}
 			requireUpdate = false;
@@ -143,8 +281,8 @@
 					return;
 				}
 
-				const tr = $('.woocommerce-shipping-totals, .wp-block-woocommerce-checkout-shipping-methods-block');
-				tr.block(blockArgs);
+				const el = $('.woocommerce-shipping-totals, .wp-block-woocommerce-checkout-shipping-methods-block');
+				el.block(blockArgs);
 
 				// Ajax select pick up point
 				$.post(
@@ -156,10 +294,10 @@
 				).error(
 					function (data) {
 						console.error(data);
-						tr.unblock();
+						el.unblock();
 					}
 				).done(function () {
-					tr.unblock();
+					el.unblock();
 					utility.renderSelectedPickUpPoint(pickUpPoint);
 				});
 				selectedPickUpPoint = pickUpPoint;
@@ -182,40 +320,46 @@
 			pickUpPoints = response.pick_up_points;
 
 			utility.renderSelectedPickUpPoint(selectedPickUpPoint);
-
-			const el = document.querySelector('pick-up-points-modal');
-			el.setPickUpPoints(pickUpPoints, handlers.selectPickUpPointHandler);
+			modalEl.setPickUpPoints(pickUpPoints, handlers.selectPickUpPointHandler);
 			pickerEl.unblock();
 		}
 	};
 
-	const init = function () {
-		// Set items
-		pickerEl = $('.bring-fraktguiden-pick-up-point-picker').first().clone();
+	// Define the custom element
+	customElements.define('pick-up-points-modal', PickUpPointsModal);
 
-		// Bind modal clicks and key-presses
-		modalEl.setPickUpPoints(
-			_fraktguiden_data.pick_up_points,
-			handlers.selectPickUpPointHandler
-		);
-	};
+	// Create modal
+	const modalEl = document.createElement('pick-up-points-modal');
+	document.body.appendChild(modalEl);
 
+	// Bind modal clicks and key-presses
+	modalEl.setPickUpPoints(
+		_fraktguiden_data.pick_up_points,
+		handlers.selectPickUpPointHandler
+	);
 
 	/**
 	 * Block checkout
 	 */
 	const blockCheckout = function (e) {
-		init();
+		// Set items
+		pickerEl = $('.bring-fraktguiden-pick-up-point-picker').first().clone();
 
 		const shippingOptionsEl = e.detail.element;
 		const inputs = $(shippingOptionsEl).find('input')
 		const shippingRates = utility.getShippingRates();
 
-		const getPicker = function(rate) {
+		/**
+		 * Get picker on shippingRate
+		 * @param rate
+		 * @returns {*}
+		 */
+		const getPicker = function (rate) {
 			const inputEl = $('[value="' + rate.rate_id + '"]')
 			const control = inputEl.parent();
 			pickerEl = control.find('.bring-fraktguiden-pick-up-point-picker');
 			if (!pickerEl.length) {
+				// Create a new element if the picker is not found
 				pickerEl = $('.bring-fraktguiden-pick-up-point-picker').first().clone();
 				pickerEl.find('.bfg-pup__change').on('click', () => modalEl.open());
 				control.append(pickerEl);
@@ -233,25 +377,6 @@
 			getPicker(shippingRate).show();
 		}
 
-
-		// Bind callback to the set-selected-shipping-rate action
-		// This is triggered when selecting a shipping option
-		wp.hooks.addAction(
-			'woocommerce_blocks-checkout-set-selected-shipping-rate',
-			'bring-fraktguiden-for-woocommerce',
-			function () {
-				console.log('updated')
-			}
-		);
-
-		// 26712 - wrong list?
-		wp.hooks.addAction(
-			'experimental__woocommerce_blocks-checkout-set-selected-shipping-rate',
-			'bring-fraktguiden-for-woocommerce',
-			function () {
-				console.log('updated')
-			}
-		);
 		let timeout = undefined;
 
 		monitorNetworkRequests(() => {
@@ -263,245 +388,80 @@
 
 		let monitorTimeout = undefined;
 		let shippingKey = utility.getShippingKey();
-		wp.data.subscribe(
+		wp.data.subscribe(() => {
+			if (window.localStorage.getItem('WOOCOMMERCE_CHECKOUT_IS_CUSTOMER_DATA_DIRTY') === 'true') {
+				// Don't do me dirty
+				return;
+			}
+
+			const currentShippingKey = utility.getShippingKey();
+			if (shippingKey === currentShippingKey) {
+				return;
+			}
+			shippingKey = currentShippingKey;
+			requireUpdate = true;
+
+			// Give the monitor a time limit to detect changes
+			if (monitorTimeout) {
+				clearTimeout(monitorTimeout);
+			}
+			monitorTimeout = setTimeout(
+				function () {
+					if (!requireUpdate) {
+						return;
+					}
+					console.warn('Timeout on detect wc/store/batch update');
+					utility.refreshPickUpPoints();
+				},
+				5000
+			);
+		});
+
+		// Ensure pick-up points are updated whenever WooCommerce triggers a reload in the checkout.
+		// This can occur when the country is changed or if changes are made after `refreshPickUpPoints`
+		// has started but before it finishes. We use a timeout to debounce updates and avoid conflicts.
+		document.addEventListener(
+			'bfg-block-shipping-rates-updating',
 			function () {
-				if (window.localStorage.getItem('WOOCOMMERCE_CHECKOUT_IS_CUSTOMER_DATA_DIRTY') === 'true') {
-					// Don't do me dirty
-					return;
+				if (refreshTimeout) {
+					clearTimeout(refreshTimeout);
 				}
-
-				const key = utility.getShippingKey();
-				if (shippingKey === key) {
-					return;
-				}
-				shippingKey = key;
-				requireUpdate = true;
-
-				// Give the monitor a time limit to detect changes
-				if (monitorTimeout) {
-					clearTimeout(monitorTimeout);
-				}
-				monitorTimeout = setTimeout(
-					function () {
-						if (! requireUpdate) {
-							return;
-						}
-						console.warn('Timeout on detect wc/store/batch update');
-						utility.refreshPickUpPoints();
-					},
-					5000
-				);
+				refreshTimeout = setTimeout(utility.refreshPickUpPoints, 1000);
 			}
 		);
 
+		/**
+		 * Monitor all state changes
+		 * React to changes in selected rate
+		 */
 		wp.data.subscribe(
 			function () {
 				let rates = utility.getShippingRates();
-				let showPicker = false;
 				for (let i = 0; i < rates.length; i++) {
 					const rate = rates[i];
-					if (! rate.selected) {
-						continue;
-					}
-					if (rate.method_id !== 'bring_fraktguiden') {
+					if (!rate.selected || rate.method_id !== 'bring_fraktguiden') {
+						// Not selected, or not a bring method
 						continue;
 					}
 
-					if (! utility.usesPickUpPoint(rate.rate_id)) {
+					if (!utility.usesPickUpPoint(rate.rate_id)) {
+						// Doesn't support pick up points
 						continue;
 					}
+					// Selected rate supports pick up points
 					getPicker(rate).show();
 					requireUpdate = true;
-					showPicker = true;
-					break;
-				}
-
-				if (! showPicker) {
-					pickerEl.hide();
 					return;
 				}
-			}
-		);
 
-		// The checkout block is empty on load and it takes a few seconds to load before it's ready.
-		// Poll to check for the existance of the woocommerce shipping option or label element.
-		// let pollInterval = setInterval(
-		// 	function () {
-		// 		const el = $('.wc-block-components-shipping-rates-control');
-		// 		if (! el.length) {
-		// 			return;
-		// 		}
-		// 		const result = el.find('.wc-block-components-radio-control__option, .wc-block-components-radio-control__label-group')
-		// 		if (! result.length) {
-		// 			return;
-		// 		}
-		// 		clearInterval(pollInterval);
-		// 		console.log('render the location picker here')
-		// 		console.log(getShippingRates())
-		// 	},
-		// 	5
-		// );
-	}
-	document.addEventListener('bfg-shipping-rates-loaded', blockCheckout);
-
-	/**
-	 * Classic checkout
-	 */
-	const classicCheckout = function () {
-		let previous = $('#shipping_method .shipping_method:checked').val();
-		$(document).on(
-			'updated_checkout',
-			function (event, data) {
-				const current = $('#shipping_method .shipping_method:checked').val();
-				let changed = current !== previous;
-				if (changed) {
-					previous = current;
-				}
-				pickerEl.show();
-				pickerEl.block(blockArgs);
-
-				utility.refreshPickUpPoints();
+				// No rate selected that supports pick up points
+				pickerEl.hide();
 			}
 		);
 	}
 
-
-
-	class PickUpPointsModal extends HTMLElement {
-		constructor() {
-			super();
-			this.attachShadow({mode: 'open'});
-
-			// Create styles for the modal
-			const styles = document.createElement('style');
-			styles.textContent = _fraktguiden_data.pick_up_point_modal_css;
-
-			const el = document.createElement('div');
-			el.classList.add('bring-fraktguiden-pick-up-points-modal');
-			el.innerHTML = `
-			  <div class="bfg-pupm__wrap">
-				<div class="bfg-pupm__inner">
-					<div class="bfg-pupm__header">
-						<div class="bfg-pupm__instruction">
-							${_fraktguiden_data.i18n.MODAL_INSTRUCTIONS}
-						</div>
-						<div class="bfg-pupm__close" tabindex="0">&times;</div>
-					</div>
-					<div class="bfg-pupm__list"></div>
-				</div>
-			  </div>
-			`;
-
-			// Append styles and modal to the shadow DOM
-			this.shadowRoot.append(styles, el);
-
-			const jqEl = $(el);
-			// Create the modal structure
-			// Close modal when clicking on the close button ✖️
-			jqEl.find('.bfg-pupm__close').on('click',  (e) => {
-				e.preventDefault();
-				this.close();
-			}).on('keyup', (e) => {
-				if (e.key !== 'Enter' && e.key !== ' ') {
-					return;
-				}
-				e.preventDefault();
-				this.close();
-			})
-
-			// Close modal when clicking on backdrop or the [esc] key
-			jqEl.on('click', (e) => {
-				e.preventDefault();
-				this.close();
-			}).on('keyup', (e) => {
-				if (e.key !== 'Escape') {
-					return;
-				}
-				e.preventDefault();
-				this.close();
-			});
-
-			// Prevent closing when clicking on inner elements
-			jqEl.find('.bfg-pupm__inner').on(
-				'click',
-				function (e) {
-					e.preventDefault();
-					e.stopPropagation();
-				}
-			);
-		}
-
-		// Open the modal
-		open() {
-			const el = this.shadowRoot.querySelector('.bring-fraktguiden-pick-up-points-modal');
-			el.classList.add('open');
-
-			const listItems = $(el).find('.bfg-pupm__item')
-			// Find selected pick up point and focus it
-			let selectedItems = listItems.filter(function () {
-				return $(this).data('id') === selectedPickUpPoint.id;
-			});
-			if (!selectedItems.length) {
-				selectedItems = listItems.first().focus();
-			}
-			selectedItems.focus();
-			setTimeout(function () {
-				selectedItems.focus();
-			}, 100);
-		}
-
-		// Close the modal
-		close() {
-			this.shadowRoot.querySelector('.bring-fraktguiden-pick-up-points-modal').classList.remove('open');
-		}
-
-		setError(text) {
-			$(this.shadowRoot.querySelector('.bfg-pupm__list')).text(text);
-		}
-
-		// Add pick-up points dynamically
-		setPickUpPoints(points, callback) {
-			const listContainer = this.shadowRoot.querySelector('.bfg-pupm__list');
-			listContainer.innerHTML = '';
-
-			points.forEach((point) => {
-				const address = utility.formatAddress(point);
-				const pointEl = $(`
-					<div class="bfg-pupm__item">
-						<div class="bfg-pupm__name">${point.name}</div>
-						<div class="bfg-pupm__address">${address}</div>
-					</div>
-				`);
-				pointEl.data('id', point.id);
-				pointEl.on('click', callback(point));
-				pointEl.on('keyup', function (e) {
-					if (e.key === 'ArrowUp') {
-						clone.prev().focus();
-						e.preventDefault();
-						return;
-					}
-					if (e.key === 'ArrowDown') {
-						clone.next().focus();
-						e.preventDefault();
-						return;
-					}
-
-					if (e.key !== 'Enter' && e.key !== ' ') {
-						return;
-					}
-					callback(point)(e);
-				});
-				listContainer.appendChild(pointEl[0]);
-			});
-		}
-	}
-
-	// Define the custom element
-	customElements.define('pick-up-points-modal', PickUpPointsModal);
-
-	const checkoutBlock = document.querySelector(
-		'.wc-block-checkout'
-	);
+	// Initialise block checkout
+	document.addEventListener('bfg-block-shipping-rates-loaded', blockCheckout);
 
 	// Because WordPress is now 50% react we have to use space age technology to implement stone age methods to figure out
 	// when the element we want is ready to be interacted with...
@@ -523,32 +483,32 @@
 				if (!el || el.children.length <= 0) {
 					continue;
 				}
-				if (! loaded) {
-					document.dispatchEvent(new CustomEvent('bfg-shipping-rates-loaded', {detail: {element: el}}));
+				if (!loaded) {
+					document.dispatchEvent(new CustomEvent('bfg-block-shipping-rates-loaded', {detail: {element: el}}));
 					loaded = true;
 				}
 				const mask = document.querySelector(
 					// Trust that WooCommerce never changes this?
 					'.wc-block-components-loading-mask'
 				);
-				if (! mask) {
+				if (!mask) {
 					continue;
 				}
-				if (refreshTimeout) {
-					clearTimeout(refreshTimeout);
-				}
-				refreshTimeout = setTimeout(utility.refreshPickUpPoints, 1000);
+				document.dispatchEvent(new CustomEvent('bfg-block-shipping-rates-updating', {detail: {element: el}}));
 			}
 		}
 	);
+
+	const checkoutBlock = document.querySelector('.wc-block-checkout');
 	if (checkoutBlock) {
 		observer.observe(checkoutBlock, {childList: true, subtree: true});
 	}
 
 	let lastStartTime = 0;
+
 	function monitorNetworkRequests(callback) {
 		const observer = new PerformanceObserver((list, ob) => {
-			if (! requireUpdate) {
+			if (!requireUpdate) {
 				return;
 			}
 			const entries = list.getEntries();
@@ -556,8 +516,10 @@
 				if (entry.initiatorType !== 'fetch') {
 					continue;
 				}
-				if (! entry.name.includes('%2Fwc%2Fstore%2Fv1%2Fbatch')) {
-					console.log('wrong name: ' + entry.name)
+				if (
+					!entry.name.includes('%2Fwc%2Fstore%2Fv1%2Fbatch') &&
+					!entry.name.includes('wp-json/wc/store/v1/batch')
+				) {
 					continue;
 				}
 				if (entry.startTime <= lastStartTime) {
@@ -571,5 +533,28 @@
 
 		observer.observe({type: 'resource', buffered: true});
 	}
+
+	/**
+	 * Classic checkout
+	 */
+	(() => {
+		let previous = $('#shipping_method .shipping_method:checked').val();
+		$(document).on(
+			'updated_checkout',
+			function (event, data) {
+				const current = $('#shipping_method .shipping_method:checked').val();
+				pickerEl = $('.bring-fraktguiden-pick-up-point-picker');
+				pickerEl.find('.bfg-pup__change').on('click', () => modalEl.open());
+
+				let changed = current !== previous;
+				if (changed) {
+					previous = current;
+				}
+				pickerEl.show();
+				utility.renderSelectedPickUpPoint(selectedPickUpPoint)
+				utility.refreshPickUpPoints();
+			}
+		);
+	})();
 
 })(jQuery);
